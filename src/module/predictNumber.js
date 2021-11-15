7e9470bbe44091727dbbd96b13c6c00f81644571
lucas-morais <lucasmoraisss@gmail.com>
2021-11-15 10:09:57 -0300

    extraindo e manipulando imagem do canvas

--- src/module/predictNumber.js
+++ src/module/predictNumber.js
@@ -1,27 +1,40 @@
-// const canvas = document.querySelector('#canvas');
+/* eslint-disable no-undef */
+const canvas = document.querySelector('#canvas');
+const button = document.querySelector('#predict-button');
 
-// function catchImage() {
-//   const button = document.querySelector('#catch-button');
-//   button.addEventListener('click', () => {
-//     const imageData = canvas
-//       .getContext('2d')
-//       .getImageData(0, 0, canvas.width, canvas.height);
-//     const imageTensor = tf.browser.fromPixels(imageData, 4);
-//     const arrayImage = Array.from(imageData.data)
-//       .filter((_arr, index) => (index + 1) % 4 === 0)
-//       .map((arr) => Math.abs(255 - arr) / 255.0);
-//     const len = imageTensor.shape[0];
-//     const finalImageTensor = tf.tensor(arrayImage, [len, len]);
-//     const resizedTensor = tf.image.resizeNearestNeighbor(
-//       imageTensor,
-//       [28, 28],
-//       true
-//     );
-//     resizedTensor.dtype = 'int32';
-//     console.log(resizedTensor);
-//     console.log(imageTensor.shape);
-//     console.log(arrayImage);
-//     resizedTensor.print();
-//     tf.browser.toPixels(resizedTensor, canvas2).then(() => {});
-//   });
-// }
+async function imageToTensor() {
+  const imageDimension = 28;
+  button.addEventListener('click', async () => {
+    const imageData = canvas
+      .getContext('2d')
+      .getImageData(0, 0, canvas.width, canvas.height);
+    const imageTensor = tf.browser.fromPixels(imageData, 4);
+    const resizedTensor = tf.image.resizeNearestNeighbor(
+      imageTensor,
+      [28, 28],
+      true
+    );
+    const tensorData = await resizedTensor.data();
+    const arrayImage = tensorData
+      .filter((_arr, index) => (index + 1) % 4 === 0)
+      .map((arr) => Math.abs(255 - arr) / 255.0);
+    const finalImageTensor = tf.tensor(
+      arrayImage,
+      [imageDimension, imageDimension],
+      'float32'
+    );
+    finalImageTensor.print();
+    console.log(finalImageTensor);
+
+    // resizedTensor.print();
+  });
+}
+
+function makePrediction() {}
+
+async function predictNumber() {
+  await imageToTensor();
+  makePrediction();
+}
+
+export default predictNumber;
